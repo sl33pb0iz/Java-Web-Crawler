6654fbc523177198da0d973b3659b1b0764cea06
Doung <hoangdoung123@gmail.com>
2024-05-16 17:45:51 +0700

    Mn check thu

--- src/scripts/Main.java
+++ src/scripts/Main.java
@@ -5,6 +5,7 @@ import src.scripts.controllers.UpgradComBuilder;
 import src.scripts.controllers.WebCrawlController;
 import src.scripts.controllers.WikipediaCrawlController;
 import src.scripts.controllers.InvestopediaComController;
+import src.scripts.controllers.CoindeskController;
 
 
 import java.io.IOException;
@@ -19,9 +20,11 @@ public class Main {
         WebCrawlController crawler1 = new WikipediaCrawlController();
         WebCrawlController crawler2 = new BlockChainComController();
         WebCrawlController crawler3 = new InvestopediaComController();
+        WebCrawlController crawler4 = new CoindeskController();
         crawler.readDataFromFile();
         crawler1.readDataFromFile();
         crawler2.readDataFromFile();
         crawler3.readDataFromFile();
+        crawler4.readDataFromFile();
     }
 }
