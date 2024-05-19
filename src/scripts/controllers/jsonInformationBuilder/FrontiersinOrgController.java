1f8b4cf8b6555ea279f3ac2bc0a2062c9a828b1c
IamHanh <139488774+Hoanganhbui2110@users.noreply.github.com>
2024-05-19 17:06:15 +0700

    Crawl

---
+++ src/scripts/controllers/jsonInformationBuilder/FrontiersinOrgController.java
@@ -0,0 +1,69 @@
+package src.scripts.controllers.jsonInformationBuilder;
+
+import org.jsoup.select.Elements;
+import src.scripts.models.WebsiteInformation;
+import src.scripts.models.CategoryEnum;
+import java.net.MalformedURLException;
+import java.net.URL;
+public class FrontiersinOrgController extends WebCrawlController{
+    @Override
+    public String getLinkFile() {return "C:\\Users\\Admin\\Java-Web-Crawler\\src\\datas\\url\\FrontiersinOrgUrl";
+    }
+    @Override
+    public void buildTitle(WebsiteInformation web) {
+        Elements elements = doc.select(".MainHeader__title");
+
+        web.setTitle(elements.text());
+    }
+    @Override
+    public void buildLink(WebsiteInformation web) {
+        web.setLink(currentUrl);
+    }
+
+    @Override
+    public void buildSource(WebsiteInformation websiteInformation) {
+
+    }
+
+    @Override
+    public void buildType(WebsiteInformation web) {
+        String[] parts = currentUrl.split("/");
+        if (parts.length > 1) {
+            String type = parts[3]; // Get the second last part of the host
+            web.setType(type);
+        }
+    }
+
+    @Override
+    public void buildSummary(WebsiteInformation web) {
+
+    }
+    @Override
+    public void buildDetail(WebsiteInformation web) {
+        Elements elements = doc.select("p");
+        web.setDetailed(elements.text());
+
+    }
+    @Override
+    public void buildCreationDate(WebsiteInformation web) {
+        Elements elements = doc.select(".container-submission-status");
+        web.setCreationDate(elements.text());
+
+    }
+
+    @Override
+    public void buildAssociated(WebsiteInformation web) {
+
+    }
+
+    @Override
+    public void buildAuthor(WebsiteInformation web) {
+        Elements elements = doc.select(".teaser-heading author-name");
+        web.setAuthor(elements.text());
+
+    }
+
+    @Override
+    public void buildCategory(WebsiteInformation web) {
+    }
+}
